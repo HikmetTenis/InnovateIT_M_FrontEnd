bd4863777a9b34d4155463d1e23bcddf2b404d36
hikmettenis <hikmettenis@yahoo.com>
2024-06-26 16:05:59 -0500

    06/26

--- src/components/iflow-configure.jsx
+++ src/components/iflow-configure.jsx
@@ -1,14 +1,32 @@
 import "@ui5/webcomponents-icons/dist/AllIcons.js";
-import { Tree,TreeItem, BusyIndicator, Panel,TableColumn,Table, TableRow, TableCell, Icon, Button} from '@ui5/webcomponents-react';
+import { Toolbar,Switch, Label, BusyIndicator, FlexBox} from '@ui5/webcomponents-react';
 import React, { useEffect, useState,useRef } from 'react';
-import {getArtifacts, getArtifact, getArtifactDetails} from '../services/s-monitoring-configure'
+import {getArtifactDetails} from '../services/s-monitoring-configure'
 import Modeler from 'bpmn-js/dist/bpmn-navigated-viewer.production.min.js';
-//import Modeler from "bpmn-js/lib/Modeler";
+import { motion, AnimatePresence, useAnimationControls } from "framer-motion"
 
 export default function IflowConfigure(props) {
     const [loading, setLoading] = useState(false);
     const containerRef = useRef(null);
     const [ modeler, setModeler ] = useState(null)
+    const wrapperVariants = {
+        hidden: {
+          opacity: 0,
+          x: '5vw',
+          transition: { ease: 'easeInOut', delay: 0.1 },
+          display: "none"
+        },
+        visible: {
+          opacity: 1,
+          x: 0,
+          transition: { ease: 'easeInOut', delay: 0.4 },
+          display: "flex"
+        },
+        exit: {
+          x: '5vh',
+          transition: { ease: 'easeInOut' },
+        },
+    };
     useEffect(() => {
         setLoading(true)
         const container = containerRef.current;
@@ -39,10 +57,24 @@ export default function IflowConfigure(props) {
       }, [])
        
       return (
-        <BusyIndicator active={loading} style={{width:"100%",height:"100%"}} size="Medium">
-            <div className="modeler-parent" style={{width:"100%", height:"100%"}}>
-                <div id="modeler-container" style={{width:"100%", height:"100%"}} ref={containerRef}></div>
-            </div>
+        <BusyIndicator active={loading} style={{width:"100%", height:"100%"}} size="Medium">
+            <FlexBox direction="Column" alignItems="Stretch" fitContainer="true">
+                <motion.div style={{flex:"1 1 auto", display:"flex", flexDirection:"column"}}
+                            variants={wrapperVariants}
+                            initial="visible"
+                            animate={!loading ? 'visible' : 'hidden'}
+                            exit="exit">
+                    <Toolbar style={{marginBottom:"10px", flex:"1 1 auto"}} alignContent={{End: 'End', Start: 'Start'}}>
+                        <Switch onChange={function _a(){}} />
+                        <Label>Add every "Start Event" and "End Event"</Label>
+                        <Switch onChange={function _a(){}} />
+                        <Label>Add every "Receiver" and "Sender"</Label>
+                    </Toolbar>
+                </motion.div>
+                <div className="modeler-parent" style={{width:"100%", height:"100%"}}>
+                    <div id="modeler-container" style={{width:"100%", height:"100%"}} ref={containerRef}></div>
+                </div>
+            </FlexBox>
         </BusyIndicator>
       );
 }
--- src/components/monitoring-configure-details.jsx
+++ src/components/monitoring-configure-details.jsx
@@ -1,9 +1,9 @@
 import "@ui5/webcomponents-icons/dist/AllIcons.js";
-import { DynamicPage, DynamicPageHeader,FlexBox,Bar,Label,DynamicPageTitle,Badge,Title,Breadcrumbs,MessageStrip,BreadcrumbsItem,ObjectStatus,Button} from '@ui5/webcomponents-react';
+import { DynamicPage, DynamicPageHeader,FlexBox,Bar,Label,DynamicPageTitle,Badge,Title,Toolbar,MessageStrip,Button, Switch} from '@ui5/webcomponents-react';
 import React, { useEffect, useState,useRef } from 'react';
 import {getArtifactRuntimeDetails} from '../services/s-monitoring-configure'
 import IflowConfigure from "./iflow-configure";
-
+import { motion, AnimatePresence, useAnimationControls } from "framer-motion"
 export default function MonitoringConfigureDetails(props) {
     const [artifactRuntimeDetails, setArtifactRuntimeDetails] = useState({})
     useEffect(() => {
@@ -13,63 +13,87 @@ export default function MonitoringConfigureDetails(props) {
             setArtifactRuntimeDetails(respValue);
         })
     })
+    const wrapperVariants = {
+        hidden: {
+          opacity: 0,
+          x: '5vw',
+          transition: { ease: 'easeInOut', delay: 0.1 },
+          display: "none"
+        },
+        visible: {
+          opacity: 1,
+          x: 0,
+          transition: { ease: 'easeInOut', delay: 0.4 },
+          display: "flex"
+        },
+        exit: {
+          x: '5vh',
+          transition: { ease: 'easeInOut' },
+        },
+    };
     return (
-        <FlexBox direction="Column" alignItems="Start" fitContainer="true">
-            <Bar design="Header">
-                <Button design="Transparent" icon="slim-arrow-left" slot="startContent" ui5-button="" icon-only="" has-icon="" onClick={props.onClick}>Back</Button>
-            </Bar>
-            <DynamicPage headerContent={
-                <DynamicPageHeader>
-                    <FlexBox wrap="Wrap">
-                        <FlexBox direction="Column">
-                            <Label>Version: {props.iflow.Version}</Label>
-                            <Label>Deployed Date: {artifactRuntimeDetails.DeployedOn}</Label>
-                            <Label>Deployed By: {artifactRuntimeDetails.DeployedBy}</Label>
+        <FlexBox direction="row" alignItems="Stretch" fitContainer="true">
+            <motion.div style={{flex:"1 1 auto", height:"100%", display:"flex", flexDirection:"column"}}
+                        variants={wrapperVariants}
+                        initial="visible"
+                        animate='visible'
+                        exit="exit">
+                <Bar design="Header">
+                    <Button design="Transparent" icon="slim-arrow-left" slot="startContent" ui5-button="" icon-only="" has-icon="" onClick={props.onClick}>Back</Button>
+                </Bar>
+                <DynamicPage headerContent={
+                    <DynamicPageHeader>
+                        <FlexBox wrap="Wrap">
+                            <FlexBox direction="Column">
+                                <Label>Version: {props.iflow.Version}</Label>
+                                <Label>Deployed Date: {artifactRuntimeDetails.DeployedOn}</Label>
+                                <Label>Deployed By: {artifactRuntimeDetails.DeployedBy}</Label>
+                            </FlexBox>
+                            <span style={{width: '1rem'}} />
+                            {/* <FlexBox direction="Column">
+                                <Label>Availability:</Label>
+                                <ObjectStatus state="Success">In Stock</ObjectStatus>
+                            </FlexBox> */}
                         </FlexBox>
-                        <span style={{width: '1rem'}} />
-                        {/* <FlexBox direction="Column">
-                            <Label>Availability:</Label>
-                            <ObjectStatus state="Success">In Stock</ObjectStatus>
-                        </FlexBox> */}
-                    </FlexBox>
-                </DynamicPageHeader>}
-                headerTitle={
-                    <DynamicPageTitle 
-                        // <><Button design="Emphasized">Edit</Button>
-                        // <Button design="Transparent">Delete</Button>
-                        // <Button design="Transparent">Copy</Button>
-                        // <Button design="Transparent" icon="action"/></>
-                     
-                // breadcrumbs={
-                //     <Breadcrumbs>
-                //         <BreadcrumbsItem>Home</BreadcrumbsItem>
-                //         <BreadcrumbsItem>Page 1</BreadcrumbsItem>
-                //         <BreadcrumbsItem>Page 2</BreadcrumbsItem>
-                //         <BreadcrumbsItem>Page 3</BreadcrumbsItem>
-                //     </Breadcrumbs>} 
-                expandedContent={
-                    <MessageStrip>Information (only visible if header content is expanded)</MessageStrip>} 
-                header={
-                    <Title>{props.iflow.Name}</Title>} 
-                // navigationActions={<>
-                //     <Button design="Transparent" icon="full-screen"/>
-                //     <Button design="Transparent" icon="exit-full-screen"/>
-                //     <Button design="Transparent" icon="decline"/></>} 
-                snappedContent={
-                    <MessageStrip>Information (only visible if header content is collapsed/snapped)</MessageStrip>} 
-                subHeader={
-                    <Label>Package Name: {props.packagename}</Label>}>
-                <Badge>Status: {artifactRuntimeDetails.Status}</Badge>
-            </DynamicPageTitle>}
-                onPinnedStateChange={function _a(){}}
-                onToggleHeaderContent={function _a(){}}
-                style={{
-                    maxHeight: '700px'
-                }}>
-                    <FlexBox direction="Column" alignItems="Start" fitContainer="true">
-                        {<IflowConfigure style={{flex:"1 1 auto",height:"100%"}} iflow={props.iflow}></IflowConfigure>}
-                    </FlexBox>
-            </DynamicPage>
+                    </DynamicPageHeader>}
+                    headerTitle={
+                        <DynamicPageTitle 
+                            // <><Button design="Emphasized">Edit</Button>
+                            // <Button design="Transparent">Delete</Button>
+                            // <Button design="Transparent">Copy</Button>
+                            // <Button design="Transparent" icon="action"/></>
+                        
+                    // breadcrumbs={
+                    //     <Breadcrumbs>
+                    //         <BreadcrumbsItem>Home</BreadcrumbsItem>
+                    //         <BreadcrumbsItem>Page 1</BreadcrumbsItem>
+                    //         <BreadcrumbsItem>Page 2</BreadcrumbsItem>
+                    //         <BreadcrumbsItem>Page 3</BreadcrumbsItem>
+                    //     </Breadcrumbs>} 
+                    expandedContent={
+                        <MessageStrip>Information (only visible if header content is expanded)</MessageStrip>} 
+                    header={
+                        <Title>{props.iflow.Name}</Title>} 
+                    // navigationActions={<>
+                    //     <Button design="Transparent" icon="full-screen"/>
+                    //     <Button design="Transparent" icon="exit-full-screen"/>
+                    //     <Button design="Transparent" icon="decline"/></>} 
+                    snappedContent={
+                        <MessageStrip>Information (only visible if header content is collapsed/snapped)</MessageStrip>} 
+                    subHeader={
+                        <Label>Package Name: {props.packagename}</Label>}>
+                    <Badge>Status: {artifactRuntimeDetails.Status}</Badge>
+                </DynamicPageTitle>}
+                    onPinnedStateChange={function _a(){}}
+                    onToggleHeaderContent={function _a(){}}
+                    style={{
+                        maxHeight: '700px'
+                    }}>
+                        <FlexBox direction="Column" alignItems="Start" justifyContent="SpaceBetween" fitContainer="true">
+                            {<IflowConfigure style={{flex:"1 1 auto",height:"100%"}} iflow={props.iflow}></IflowConfigure>}
+                        </FlexBox>
+                </DynamicPage>
+            </motion.div>
         </FlexBox>
     )
 }
