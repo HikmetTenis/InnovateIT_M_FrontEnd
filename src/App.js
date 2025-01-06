1cad2631a05a47bda7c940b1d966d3f4500c5954
hikmettenis <hikmettenis@yahoo.com>
2025-01-05 19:54:49 -0600

    01/05

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import { getService } from "./services/s-account";
 function App() {
   const [message,setMessage] = useState({open:false,message:"",result:null, callback:null, toast:false})
   useEffect(() => {
-      document.title = `${process.env.REACT_APP_ENVIRONMENT}-${document.title}`;
+      document.title = `${process.env.REACT_APP_ENVIRONMENT} - ${document.title}`;
   }, []);
   return (
     
